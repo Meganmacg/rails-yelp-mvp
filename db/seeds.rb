503f3ae891a9e0739eb1fa8a2d14a0549b2e2caf
Megan Gordon <megangordon126@gmail.com>
2022-11-16 16:29:10 +0000

    restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(name: 'Padella', address: 'London Bridge', category: 'italian', phone_number: '01012123123')
+Restaurant.create(name: 'Case Croute', address: 'London Bridge', category: 'french', phone_number: '01012145123')
+Restaurant.create(name: 'Itsu', address: 'Canary Wharf', category: 'japanese', phone_number: '01056123123')
+Restaurant.create(name: 'Taste of China', address: 'Hackney', category: 'chinese', phone_number: '03332123123')
+Restaurant.create(name: 'New East House', address: 'Whitecross', category: 'chinese', phone_number: '09912123123')
